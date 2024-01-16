2588d37ae1bfd8d8fe57b2cb8f056489ddce4743
kazuhiko <k.tentaikansoku@gmail.com>
2024-01-17 01:21:12 +0900

    commit

--- script.js
+++
@@ -1,7 +0,0 @@
-const cursor = document.getElementById("cursor");
-
-document.addEventListener("mousemove", function (e) {
-    // console.log(e.clientY);
-    cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-});
-
